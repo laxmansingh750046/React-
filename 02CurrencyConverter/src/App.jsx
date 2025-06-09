c9b99bc653553ea713ca811822d8e88e9e7819d2
laxman singh <laxmansingh790616@gmail.com>
2025-06-09 06:30:56 +0530

    currency converter

--- 02CurrencyConverter/src/App.jsx
+++ 02CurrencyConverter/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {inputBox} from './components'
+import userCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='text-3xl bg-orange'>
+    <h1 className='text-3xl bg-orange-500'>
       Currency converter</h1>
     </>
   )
